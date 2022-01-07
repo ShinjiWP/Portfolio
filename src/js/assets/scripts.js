5380ac82728f6ad81c2b4c73ce5cf8f3c4690006
ShinjiWP <xkounagox@gmail.com>
2022-01-07 23:37:43 +0900

    202201072330

---
+++ src/js/assets/scripts.js
@@ -0,0 +1,24 @@
+const mediaQuery = window.matchMedia("(max-width: 1079px)");
+
+// ページが読み込まれた時に実行
+handle(mediaQuery);
+
+// ウィンドウサイズを変更しても実行（ブレイクポイントの監視）
+mediaQuery.addListener(handle);
+
+function handle(mm) {
+	if (mm.matches) {
+		// ウィンドウサイズ1079px以下のときの処理
+		jQuery("#btn1").on("click", function () {
+			jQuery(".p-products").toggleClass("open");
+		});
+	} else {
+	}
+}
+
+//以下は基本的な書き方
+// $(function () {
+// 	jQuery("#btn1").on("click", function () {
+// 		jQuery(".p-products").toggleClass("open");
+// 	});
+// });
