1d7f1ebbb888b7baae8c65ae7feef85df5e8ef73
ShinjiWP <xkounagox@gmail.com>
2022-01-16 10:25:23 +0900

    202201161025

--- src/js/assets/scripts.js
+++ src/js/assets/scripts.js
@@ -1,41 +1,43 @@
-$(".js-btn1").click(function () {
-	$(".p-header__ttl:nth-child(3),.p-header__ttl:nth-child(4),.p-header__ttl:nth-child(5)").toggleClass("js-gray");
-});
+// $(".js-btn1").on("click",function () {
+// 	$(".p-header__ttl:nth-child(3),.p-header__ttl:nth-child(4),.p-header__ttl:nth-child(5)").toggleClass("js-gray");
+// });
 
-$(".js-btn2").click(function () {
-	$(".p-header__ttl:nth-child(2),.p-header__ttl:nth-child(4),.p-header__ttl:nth-child(5)").toggleClass("js-gray");
-});
+// $(".js-btn2").on("click",function () {
+// 	$(".p-header__ttl:nth-child(2),.p-header__ttl:nth-child(4),.p-header__ttl:nth-child(5)").toggleClass("js-gray");
+// });
 
-$(".js-btn3").click(function () {
-	$(".p-header__ttl:nth-child(3),.p-header__ttl:nth-child(2),.p-header__ttl:nth-child(5)").toggleClass("js-gray");
-});
+// $(".js-btn3").on("click",function () {
+// 	$(".p-header__ttl:nth-child(3),.p-header__ttl:nth-child(2),.p-header__ttl:nth-child(5)").toggleClass("js-gray");
+// });
 
-$(".js-btn4").click(function () {
-	$(".p-header__ttl:nth-child(3),.p-header__ttl:nth-child(4),.p-header__ttl:nth-child(2)").toggleClass("js-gray");
-});
+// $(".js-btn4").on("click",function () {
+// 	$(".p-header__ttl:nth-child(3),.p-header__ttl:nth-child(4),.p-header__ttl:nth-child(2)").toggleClass("js-gray");
+// });
 
-$(".js-btn1").mouseenter(function () {
+
+
+$(".js-btn1").on("mouseenter",function () {
 	$(
 		".p-header__ttl:nth-child(2),.p-header__ttl:nth-child(3),.p-header__ttl:nth-child(4),.p-header__ttl:nth-child(5)"
 	).removeClass("js-gray");
 	$(".p-header__ttl:nth-child(3),.p-header__ttl:nth-child(4),.p-header__ttl:nth-child(5)").toggleClass("js-gray");
 });
 
-$(".js-btn2").mouseenter(function () {
+$(".js-btn2").on("mouseenter",function () {
 	$(
 		".p-header__ttl:nth-child(2),.p-header__ttl:nth-child(3),.p-header__ttl:nth-child(4),.p-header__ttl:nth-child(5)"
 	).removeClass("js-gray");
 	$(".p-header__ttl:nth-child(2),.p-header__ttl:nth-child(4),.p-header__ttl:nth-child(5)").toggleClass("js-gray");
 });
 
-$(".js-btn3").mouseenter(function () {
+$(".js-btn3").on("mouseenter",function () {
 	$(
 		".p-header__ttl:nth-child(2),.p-header__ttl:nth-child(3),.p-header__ttl:nth-child(4),.p-header__ttl:nth-child(5)"
 	).removeClass("js-gray");
 	$(".p-header__ttl:nth-child(3),.p-header__ttl:nth-child(2),.p-header__ttl:nth-child(5)").toggleClass("js-gray");
 });
 
-$(".js-btn4").mouseenter(function () {
+$(".js-btn4").on("mouseenter",function () {
 	$(
 		".p-header__ttl:nth-child(2),.p-header__ttl:nth-child(3),.p-header__ttl:nth-child(4),.p-header__ttl:nth-child(5)"
 	).removeClass("js-gray");
