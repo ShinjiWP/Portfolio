3f5e6718de90d3c1ee93eebd0bee5a8d2556ace0
ShinjiWP <xkounagox@gmail.com>
2022-01-19 23:21:19 +0900

    202201192320

--- src/js/assets/scripts.js
+++ src/js/assets/scripts.js
@@ -19,6 +19,7 @@ $(".js-btn1").on("mouseenter", function () {
 		".p-header__ttl:nth-child(2),.p-header__ttl:nth-child(3),.p-header__ttl:nth-child(4),.p-header__ttl:nth-child(5)"
 	).removeClass("js-gray");
 	$(".p-header__ttl:nth-child(3),.p-header__ttl:nth-child(4),.p-header__ttl:nth-child(5)").toggleClass("js-gray");
+	$(".p-header__ttl:nth-child(5) span").css({ "background-color": "gray" });
 });
 
 $(".js-btn2").on("mouseenter", function () {
@@ -26,6 +27,8 @@ $(".js-btn2").on("mouseenter", function () {
 		".p-header__ttl:nth-child(2),.p-header__ttl:nth-child(3),.p-header__ttl:nth-child(4),.p-header__ttl:nth-child(5)"
 	).removeClass("js-gray");
 	$(".p-header__ttl:nth-child(2),.p-header__ttl:nth-child(4),.p-header__ttl:nth-child(5)").toggleClass("js-gray");
+	$(".p-header__ttl:nth-child(5) span").css({ "background-color": "gray" });
+	$(".p-header__ttl:nth-child(2) span").css({ color: "gray" });
 });
 
 $(".js-btn3").on("mouseenter", function () {
@@ -33,6 +36,8 @@ $(".js-btn3").on("mouseenter", function () {
 		".p-header__ttl:nth-child(2),.p-header__ttl:nth-child(3),.p-header__ttl:nth-child(4),.p-header__ttl:nth-child(5)"
 	).removeClass("js-gray");
 	$(".p-header__ttl:nth-child(3),.p-header__ttl:nth-child(2),.p-header__ttl:nth-child(5)").toggleClass("js-gray");
+	$(".p-header__ttl:nth-child(5) span").css({ "background-color": "gray" });
+	$(".p-header__ttl:nth-child(2) span").css({ color: "gray" });
 });
 
 $(".js-btn4").on("mouseenter", function () {
@@ -40,12 +45,16 @@ $(".js-btn4").on("mouseenter", function () {
 		".p-header__ttl:nth-child(2),.p-header__ttl:nth-child(3),.p-header__ttl:nth-child(4),.p-header__ttl:nth-child(5)"
 	).removeClass("js-gray");
 	$(".p-header__ttl:nth-child(3),.p-header__ttl:nth-child(4),.p-header__ttl:nth-child(2)").toggleClass("js-gray");
+	$(".p-header__ttl:nth-child(5) span").css({ color: "gray" });
+	$(".p-header__ttl:nth-child(2) span").css({ color: "gray" });
 });
 
 $(".js-btn1,.js-btn2,.js-btn3,.js-btn4").on("mouseout", function () {
 	$(
 		".p-header__ttl:nth-child(2),.p-header__ttl:nth-child(3),.p-header__ttl:nth-child(4),.p-header__ttl:nth-child(5)"
 	).removeClass("js-gray");
+	$(".p-header__ttl:nth-child(5) span").css({ "background-color": "#00ff00" });
+	$(".p-header__ttl:nth-child(2) span").css({ color: "#ff0000" });
 });
 
 $(".p-nav__btn").on("click", function () {
