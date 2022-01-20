ef4c89c65aa5e9f2125ddce88764ee7dcbe9fb89
ShinjiWP <xkounagox@gmail.com>
2022-01-20 21:44:11 +0900

    202201202145

--- src/js/assets/dot.js
+++ src/js/assets/dot.js
@@ -2,7 +2,7 @@ gsap.set(".c-dot", { width: 14, height: 14, borderRadius: "20%", backgroundColor
 
 const tl = gsap.timeline({ repeat: -1, repeatRefresh: true });
 
-tl.to(".c-dot", { rotate: "random(0, 360)", duration: 3, ease: "back.out" });
-tl.to(".c-dot", { rotate: "random(-360, 0)", duration: 3, ease: "back.out" });
+tl.to(".c-dot", { rotate: "random(0, 360)", duration: 2.5, ease: "back.out" });
+tl.to(".c-dot", { rotate: "random(-360, 0)", duration: 2.5, ease: "back.out" });
 
 
