06b852aa9eb286aee033c9ad399b474fe7fcb997
ShinjiWP <xkounagox@gmail.com>
2022-01-21 18:37:29 +0900

    202201211835

--- src/js/develop/fade.js
+++ src/js/develop/fade.js
@@ -1,10 +1,20 @@
-$(".load-text").fadeOut(7000, function () {
-	$(".loading").toggleClass("open");
+$(".load-text").fadeOut(5000, function () {
+	$(".loading").toggleClass("open"); //←ここだけ残せば初期使用でローディングと分離できる
+	$(".p-header__ttl:nth-child(2)").css({ transform: "translateX(0%)", opacity: "1" });
+	$(".p-header__ttl:nth-child(3)")
+		.delay(30)
+		.queue(function () {
+			$(this).css({ transform: "translateX(0%)", opacity: "1" });
+		});
+	$(".p-header__ttl:nth-child(4)")
+		.delay(40)
+		.queue(function () {
+			$(this).css({ transform: "translateX(0%)", opacity: "1" });
+		});
+	$(".p-header__ttl:nth-child(5)")
+		.delay(50)
+		.queue(function () {
+			$(this).css({ transform: "translateX(0%)", opacity: "1" });
+		});
 });
 
-// $(function () {
-// 	setTimeout(function () {
-// 		// $(".load-text").fadeOut(7000);
-// 		$(".loading").toggleClass("open");
-// 	},7000);
-// });
