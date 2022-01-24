b32ecee8aa0d445c7af1b1319de1a63f3acb425b
ShinjiWP <xkounagox@gmail.com>
2022-01-24 18:00:26 +0900

    202201241800

--- src/js/assets/scripts.js
+++ src/js/assets/scripts.js
@@ -67,21 +67,21 @@ $(".p-nav__btn").on("click", function () {
 
 $(".load-text").fadeOut(5000, function () {
 	$(".loading").toggleClass("open"); //←ここだけ残せば初期使用でローディングと分離できる
-	$(".p-header__ttl:nth-child(2)").css({ transform: "translateX(0%)", opacity: "1" });
-	$(".p-header__ttl:nth-child(3)")
-		.delay(30)
-		.queue(function () {
-			$(this).css({ transform: "translateX(0%)", opacity: "1" });
-		});
-	$(".p-header__ttl:nth-child(4)")
-		.delay(40)
-		.queue(function () {
-			$(this).css({ transform: "translateX(0%)", opacity: "1" });
-		});
-	$(".p-header__ttl:nth-child(5)")
-		.delay(50)
-		.queue(function () {
-			$(this).css({ transform: "translateX(0%)", opacity: "1" });
-		});
+	// $(".p-header__ttl:nth-child(2)").css({ transform: "translateX(0%)", opacity: "1" });
+	// $(".p-header__ttl:nth-child(3)")
+	// 	.delay(30)
+	// 	.queue(function () {
+	// 		$(this).css({ transform: "translateX(0%)", opacity: "1" });
+	// 	});
+	// $(".p-header__ttl:nth-child(4)")
+	// 	.delay(40)
+	// 	.queue(function () {
+	// 		$(this).css({ transform: "translateX(0%)", opacity: "1" });
+	// 	});
+	// $(".p-header__ttl:nth-child(5)")
+	// 	.delay(50)
+	// 	.queue(function () {
+	// 		$(this).css({ transform: "translateX(0%)", opacity: "1" });
+	// 	});
 });
 
