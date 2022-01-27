3423dcadc8c93429d40ef64fb7aaa099c3c7a8bf
ShinjiWP <xkounagox@gmail.com>
2022-01-27 17:12:42 +0900

    202201271710

--- src/js/develop/button.js
+++ src/js/develop/button.js
@@ -14,43 +14,45 @@
 // 	$(".p-header__ttl:nth-child(3),.p-header__ttl:nth-child(4),.p-header__ttl:nth-child(2)").toggleClass("js-gray");
 // });
 
-$(".js-btn1").on("mouseenter", function () {
+
+
+$(".c-hover--btn1").on("mouseenter", function () {
 	$(
 		".p-header__ttl:nth-child(2),.p-header__ttl:nth-child(3),.p-header__ttl:nth-child(4),.p-header__ttl:nth-child(5)"
 	).removeClass("js-gray");
 	$(".p-header__ttl:nth-child(3),.p-header__ttl:nth-child(4),.p-header__ttl:nth-child(5)").toggleClass("js-gray");
 	$(".p-header__ttl:nth-child(5) span").css({ "background-color": "gray" });
-	// $(".js-btn2, .js-btn3, .js-btn4").toggleClass("flt-btn");
+	$(".js-btn2, .js-btn3, .js-btn4").toggleClass("js-filter");
 });
 
-$(".js-btn2").on("mouseenter", function () {
+$(".c-hover--btn2").on("mouseenter", function () {
 	$(
 		".p-header__ttl:nth-child(2),.p-header__ttl:nth-child(3),.p-header__ttl:nth-child(4),.p-header__ttl:nth-child(5)"
 	).removeClass("js-gray");
 	$(".p-header__ttl:nth-child(2),.p-header__ttl:nth-child(4),.p-header__ttl:nth-child(5)").toggleClass("js-gray");
 	$(".p-header__ttl:nth-child(5) span").css({ "background-color": "gray" });
 	$(".p-header__ttl:nth-child(2) span").css({ color: "gray" });
-	// $(".js-btn1, .js-btn3, .js-btn4").toggleClass("flt-btn");
+	$(".js-btn1, .js-btn3, .js-btn4").toggleClass("js-filter");
 });
 
-$(".js-btn3").on("mouseenter", function () {
+$(".c-hover--btn3").on("mouseenter", function () {
 	$(
 		".p-header__ttl:nth-child(2),.p-header__ttl:nth-child(3),.p-header__ttl:nth-child(4),.p-header__ttl:nth-child(5)"
 	).removeClass("js-gray");
 	$(".p-header__ttl:nth-child(3),.p-header__ttl:nth-child(2),.p-header__ttl:nth-child(5)").toggleClass("js-gray");
 	$(".p-header__ttl:nth-child(5) span").css({ "background-color": "gray" });
 	$(".p-header__ttl:nth-child(2) span").css({ color: "gray" });
-	// $(".js-btn2, .js-btn1, .js-btn4").toggleClass("flt-btn");
+	$(".js-btn2, .js-btn1, .js-btn4").toggleClass("js-filter");
 });
 
-$(".js-btn4").on("mouseenter", function () {
+$(".c-hover--btn4").on("mouseenter", function () {
 	$(
 		".p-header__ttl:nth-child(2),.p-header__ttl:nth-child(3),.p-header__ttl:nth-child(4),.p-header__ttl:nth-child(5)"
 	).removeClass("js-gray");
 	$(".p-header__ttl:nth-child(3),.p-header__ttl:nth-child(4),.p-header__ttl:nth-child(2)").toggleClass("js-gray");
 	$(".p-header__ttl:nth-child(5) span").css({ color: "gray" });
 	$(".p-header__ttl:nth-child(2) span").css({ color: "gray" });
-	// $(".js-btn2, .js-btn3, .js-btn1").toggleClass("flt-btn");
+	$(".js-btn2, .js-btn3, .js-btn1").toggleClass("js-filter");
 });
 
 $(".js-btn1,.js-btn2,.js-btn3,.js-btn4").on("mouseout", function () {
@@ -59,5 +61,5 @@ $(".js-btn1,.js-btn2,.js-btn3,.js-btn4").on("mouseout", function () {
 	).removeClass("js-gray");
 	$(".p-header__ttl:nth-child(5) span").css({ "background-color": "#00ff00" });
 	$(".p-header__ttl:nth-child(2) span").css({ color: "#ff0000" });
-	// $(".js-btn1,.js-btn2,.js-btn3,.js-btn4").removeClass("flt-btn");
+	$(".js-btn1,.js-btn2,.js-btn3,.js-btn4").removeClass("js-filter");
 });
